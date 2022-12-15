9a89b2eb8a0daac101253cecff9b0a00002846df
mikhita <mikheilispiriani@yahoo.com>
2022-12-16 01:14:26 +0400

    adding some styles and html

--- src/index.js
+++ src/index.js
@@ -33,10 +33,10 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <App />
-      </PersistGate>
-    </Provider>
+    {/* <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}> */}
+    <App />
+    {/* </PersistGate>
+    </Provider> */}
   </React.StrictMode>
 );
